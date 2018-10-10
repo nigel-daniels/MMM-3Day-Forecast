ae89ae6f68602752df180d2603969a6e10e0b384
yohaybn <yohaybn@gmail.com>
2018-10-10 09:05:41 +0000

    cahnge forecastIcon to weathericon

--- MMM-3Day-Forecast.js
+++ MMM-3Day-Forecast.js
@@ -19,6 +19,45 @@ Module.register('MMM-3Day-Forecast', {
 		showWind: true,
 		horizontalView: false,
 		interval: 900000 // Every 15 mins
+		codeIconTable: {
+			  "201": "wi-thunderstorm",
+			  "202": "wi-thunderstorm",
+			  "230": "wi-storm-showers",
+			  "231": "wi-storm-showers",
+			  "232": "wi-storm-showers",
+			  "233": "wi-thunderstorm",
+			  "300": "wi-sprinkle",
+			  "301": "wi-sprinkle",
+			  "302": "wi-sprinkle",
+			  "500": "wi-showers",
+			  "501": "wi-showers",
+			  "502": "wi-showers",
+			  "511": "wi-showers",
+			  "520": "wi-rain",
+			  "521": "wi-rain",
+			  "522": "wi-rain",
+			  "600": "wi-snow",
+			  "601": "wi-snow",
+			  "602": "wi-snow",
+			  "610": "wi-rain-mix",
+			  "611": "wi-sleet",
+			  "612": "wi-sleet",
+			  "621": "wi-snow",
+			  "622": "wi-snow",
+			  "623": "wi-rain",
+			  "700": "wi-fog",
+			  "711": "wi-smoke",
+			  "721": "wi-smog",
+			  "731": "wi-dust",
+			  "741": "wi-fog",
+			  "751": "wi-fog",
+			  "800": "wi-day-sunny",
+			  "801": "wi-day-cloudy",
+			  "802": "wi-day-cloudy",
+			  "803": "wi-day-cloudy",
+			  "804": "wi-cloudy",
+			  "900": "wi-na"
+		},
 	},
 
 
@@ -261,12 +300,9 @@ Module.register('MMM-3Day-Forecast', {
 					forcastTitle.className = 'forecastTitle';
 					forcastTitle.innerHTML = title;
 
-					forecastIcon = document.createElement('img');
-					forecastIcon.className = 'forecastIcon';
-					forecastIcon.setAttribute('height', '50');
-					forecastIcon.setAttribute('width', '50');
-					forecastIcon.src = './modules/MMM-3Day-Forecast/images/' + this.forecast[i].icon + '.gif';
-
+					forecastIcon = document.createElement('i');
+					forecastIcon.className = 'wi ' + this.config.codeIconTable[this.forecast[i].code];
+					
 					forecastText = document.createElement('div');
 					forecastText.className = 'forecastText horizontalView bright';
 
